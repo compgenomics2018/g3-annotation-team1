2036feabde10a4bed7791c28288345e65818bc51
Gulati_Saurabh_sgulati36 <saurabhg59@gatech.edu>
2018-04-07 17:53:05 -0400

    merged the fasta reformatting scripts into 1 script

--- reformatFasta.py
+++ reformatFasta.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python
 
+import re
 import sys
+from subprocess import check_call
 
 inFile=sys.argv[1]
-outFile=inFile+".reformatted"
-test=0
+outFile=inFile+'_reformatted'
 header=""
 seq=""
+test=0
+data=[]
+
+with open(inFile,"r") as INPUT:
+	fasta=INPUT.read().strip().split("\n")
+
+for line in fasta:
+	if(">" in line):
+		if(test!=0):
+			data.append(header)
+			data.append(seq)
+		header=line
+		seq=""
+		test=1
+	else:
+		seq+=line
+data.append(header)
+data.append(seq)
+
+for i in range(len(data)):
+	if('>' in data[i]):
+		if('>gene' in data[i]):
+			g=re.search('(>gene_\d+).*',data[i])
+			data[i]=">"+inFile+"_"+g.group(1)[1:]
+		else:
+			temp=data[i].split(" ")
+			temp2=temp[8].split(";")
+			m=re.search("ID=(\d+_\d+)",temp2[0])
+			data[i]=">"+inFile+"_"+"prodigal_"+m.group(1)
 
 with open(outFile,"w+") as OUTPUT:
-	with open(inFile,"r") as INPUT:
-		for line in INPUT:
-			if(test==0):
-				if(">" in line):
-					header=line
-					seq=""
-					test=1
-			else:
-				if (">" in line):
-					OUTPUT.write(header+seq+"\n")
-					header=line
-					seq=""
-					test=1
-				else:
-					seq+=line.strip()
-	OUTPUT.write(header+seq+"\n")
+	OUTPUT.write("\n".join(data))
+	OUTPUT.write("\n")
+
+check_call(["sed","-i","s/\.f[na]a//g",outFile])
+
+if(".faa" in inFile):
+	check_call(["sed","-ir","s/\*$//g",outFile])
