322fabbaf9a2d0022d82a2388843e2af7895a846
saurabhg59 <saurabhg59@gmail.com>
2018-04-10 05:02:39 -0400

    finished outputParser.py

--- outputParser.py
+++ outputParser.py
@@ -28,6 +28,18 @@ parser.add_option("-u", "--uclust", action="store", type="string", dest="uc_File
 parser.add_option("-g", "--mergedgff", action="store", type="string", dest="gff_File",
                   help="merged gff file")
 
+parser.add_option("-r", "--door2", action="store", type="string", dest="door2_File",
+                  help="door2 output file")
+
+parser.add_option("-c", "--card", action="store", type="string", dest="card_File",
+                  help="card output file")
+
+parser.add_option("-v", "--vfdb", action="store", type="string", dest="vfdb_File",
+                  help="vfdb output file")
+
+parser.add_option("-o", "--outdir", action="store", type="string", dest="outdir",
+                  help="final output directory")
+
 (options, args) = parser.parse_args()
 
 def LipoP(inFile):
@@ -142,15 +154,45 @@ def uclust(inFile):
 
 	return(index)
 
+def door2(inFile):
+	with open(inFile,"r") as INPUT:
+		data=INPUT.read().strip().split("\n")
+
+	for i in range(len(data)):
+		temp=data[i].split("\t")
+		data[i]=[temp[0],"door2","target="+temp[1]+";start="+temp[2]+";stop="+temp[3]+";score="+temp[4]+";percentID="+temp[5]+";coverage="+temp[6]+";operonID="+temp[7]+";COG="+temp[8]+";"]
+
+	return(data)
+
+def card(inFile):
+	with open(inFile,"r") as INPUT:
+		data=INPUT.read().strip().split("\n")
+
+	for i in range(len(data)):
+		temp=data[i].split("\t")
+		data[i]=[temp[0],"card","start="+temp[1]+";stop="+temp[2]+";queryLength="+temp[3]+";alignLength="+temp[4]+";coverage="+temp[5]+";percentID="+temp[6]+";e-value="+temp[7]+";stitle="+temp[8]+";"]
+
+	return data
+
+def vfdb(inFile):
+	with open(inFile,"r") as INPUT:
+		data=INPUT.read().strip().split("\n")
+
+	for i in range(len(data)):
+		temp=data[i].split("\t")
+		data[i]=[temp[0],"vfdb","start="+temp[1]+";stop="+temp[2]+";queryLength="+temp[3]+";alignLength="+temp[4]+";coverage="+temp[5]+";percentID="+temp[6]+";e-value="+temp[7]+";stitle="+temp[8]+";"]
+
+	return data
 
 lipoOutput=LipoP(options.LipoP_File)
 deepArgOutput=deepArg(options.deepArg_File)
-
-
 eggnogOutput=eggDia(options.eggnogDiamond_File)
 tmhmmOutput=tmhmm(options.tmhmm_File)
 signalpOutput=signalp(options.signalp_File)
 phobiusOutput=phobius(options.phobius_File)
+door2Output=door2(options.door2_File)
+cardOutput=card(options.card_File)
+vfdbOutput=vfdb(options.vfdb_File)
 
 clusterIndex=uclust(options.uc_File)
 
@@ -189,6 +231,38 @@ for i in deepArgOutput:
 		data[m.group(1)][m.group(2)][i[1]]=i[2]
 
 del deepArgOutput
+
+for i in cardOutput:
+	m=re.search("(SRR\d+)_(.*)",i[0])
+
+	if(m.group(1) in data):
+		if(m.group(2) in data[m.group(1)]):
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+		else:
+			data[m.group(1)][m.group(2)]={}
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+	else:
+		data[m.group(1)]={}
+		data[m.group(1)][m.group(2)]={}
+		data[m.group(1)][m.group(2)][i[1]]=i[2]
+
+del cardOutput
+
+for i in vfdbOutput:
+	m=re.search("(SRR\d+)_(.*)",i[0])
+
+	if(m.group(1) in data):
+		if(m.group(2) in data[m.group(1)]):
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+		else:
+			data[m.group(1)][m.group(2)]={}
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+	else:
+		data[m.group(1)]={}
+		data[m.group(1)][m.group(2)]={}
+		data[m.group(1)][m.group(2)][i[1]]=i[2]
+
+del vfdbOutput
 
 for i in eggnogOutput:
 	for j in clusterIndex[i[0]]:
@@ -311,8 +385,37 @@ for i in phobiusOutput:
 
 del phobiusOutput
 
-#### opening the gff files and reading all useful info
+for i in door2Output:
+	for j in clusterIndex[i[0]]:
+		m=re.search("(SRR\d+)_(.*)",j)
 
+		if(m.group(1) in data):
+			if(m.group(2) in data[m.group(1)]):
+				data[m.group(1)][m.group(2)][i[1]]=i[2]
+			else:
+				data[m.group(1)][m.group(2)]={}
+				data[m.group(1)][m.group(2)][i[1]]=i[2]
+		else:
+			data[m.group(1)]={}
+			data[m.group(1)][m.group(2)]={}
+			data[m.group(1)][m.group(2)][i[1]]=i[2]		
+
+	m=re.search("(SRR\d+)_(.*)",i[0])
+
+	if(m.group(1) in data):
+		if(m.group(2) in data[m.group(1)]):
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+		else:
+			data[m.group(1)][m.group(2)]={}
+			data[m.group(1)][m.group(2)][i[1]]=i[2]
+	else:
+		data[m.group(1)]={}
+		data[m.group(1)][m.group(2)]={}
+		data[m.group(1)][m.group(2)][i[1]]=i[2]
+
+del door2Output
+
+#### opening the gff files and reading all useful info
 
 with open(options.gff_File,"r") as INPUT:
 	lines=INPUT.read().strip().split("\n")
@@ -344,3 +447,12 @@ for line in lines:
 del lines
 
 #### writing everything into 1 gff file per sample
+
+for i in data:
+	print(i)
+	with open(options.outdir+"/"+i+".gff","w+") as OUTPUT:
+		for j in data[i]:
+			# print(j)
+			for k in data[i][j]:
+				# print(k)
+				OUTPUT.write(j+"\t"+k+"\t"+"\t".join(info[i][j])+"\t"+data[i][j][k]+"\n")
